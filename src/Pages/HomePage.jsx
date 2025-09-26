9cc6fba81c30172da4e0b09ed8ff2e591028a10f
Mayur Tarare <mandartarare44@gmail.com>
2025-09-26 12:25:59 +0530

    folders and files

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+import Home from '../Components/Home'
+import Footer from '../Components/Footer'
+
+
+
+
+
+const HomePage = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Home/>
+      <Footer/>
+      
+    </div>
+  )
+}
+
+export default HomePage
